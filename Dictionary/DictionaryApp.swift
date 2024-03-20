27a96df6a8b6fdd05885e4d5bd124bb748058ab0
Andrew Styles <a.v.styles>
2024-03-20 13:38:52 +0000

    Initial Commit

---
+++ Dictionary/DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryApp.swift
+//  Dictionary
+//
+//  Created by Andrew Styles on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
